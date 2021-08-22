448eac8445c82a5edd59d80cc4dab5b9b92e9cff
Michael Donlan <mdonlan8792@yahoo.com>
2021-08-21 22:44:39 -0400

    working on creating ast

--- json_parser/main.cpp
+++ json_parser/main.cpp
@@ -12,11 +12,14 @@
 
 enum Token_Type {
 	OPEN_BRACKET,
-	STRING
+	STRING_VALUE,
+	NAME
 };
 
 struct Token {
 	Token_Type type;
+	
+	std::string str;
 	
 //	union {
 //		float number;
@@ -35,7 +38,8 @@ struct Parser {
 
 
 void consume(Parser* parser);
-void peek(Parser* parser);
+char peek(Parser* parser, unsigned int index);
+void eat_whitespace(Parser* parser);
 
 int main(int argc, const char * argv[]) {
 	Parser* parser = new Parser;
@@ -59,33 +63,58 @@ int main(int argc, const char * argv[]) {
 }
 
 void consume(Parser* parser) {
+	eat_whitespace(parser);
 	char c = parser->str[parser->index];
 	
 	// match against tokens
 	if (c == '\0') {
 		parser->eof = true;
-	} else if (c == '{') {
+	} else if (c == '{') { // left bracket token
 		Token token;
 		token.type = OPEN_BRACKET;
 		parser->tokens.push_back(token);
-		parser->index++;
-	} else if (c == '"') {
+	} else if (c == '"') { // name/decl token or string value token
 		Token token;
-		token.type = STRING;
+//		token.type = STRING;
 		
 		std::string str;
 		unsigned int p_index = parser->index;
-		bool finding_end_of_string = true;
+		bool found_end_of_string = false;
 		
-		while (finding_end_of_string) {
-//			char pc =
+		while (!found_end_of_string) {
+			char pc = peek(parser, p_index);
+			if (pc == '"') {
+				found_end_of_string = true;
+				eat_whitespace(parser);
+				if (peek(parser, p_index + 1) == ':') {
+					// this token is a name/declaration token
+					token.type = NAME;
+				} else {
+					// this token is a string value
+					token.type = STRING_VALUE;
+				}
+			} else {
+				str.push_back(pc);
+				p_index++;
+			}
 		}
 		
-	} else {
-		parser->index++;
+		token.str = str;
+		
+		parser->tokens.push_back(token);
 	}
+	
+	parser->index++;
 }
 
-void peek(Parser* parser) {
-	
+char peek(Parser* parser, unsigned int index) {
+	return parser->str[index];
+}
+
+void eat_whitespace(Parser* parser) {
+	char c = parser->str[parser->index];
+	while (c == ' ' || c == '\n' || c == '\t') {
+		parser->index++;
+		c = parser->str[parser->index];
+	}
 }
